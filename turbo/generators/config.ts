94ce20de88787784bc44a5726abe7048ebc3ce20
bnm.wag@gmail.com <b.wagner@zap.st>
2024-01-15 20:30:45 +0100

    add graphql schema to code gen

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,50 +1,8 @@
-// import { PlopTypes } from "@turbo/gen";
-
-// export default function generator(plop: PlopTypes.NodePlopAPI): void {
-//   plop.setGenerator("example", {
-//     description:
-//       "An example Turborepo generator - creates a new file at the root of the project",
-//     prompts: [
-//       {
-//         type: "input",
-//         name: "file",
-//         message: "What is the name of the new file to create?",
-//         validate: (input: string) => {
-//           if (input.includes(".")) {
-//             return "file name cannot include an extension";
-//           }
-//           if (input.includes(" ")) {
-//             return "file name cannot include spaces";
-//           }
-//           if (!input) {
-//             return "file name is required";
-//           }
-//           return true;
-//         },
-//       },
-//       {
-//         type: "list",
-//         name: "type",
-//         message: "What type of file should be created?",
-//         choices: [".md", ".txt"],
-//       },
-//       {
-//         type: "input",
-//         name: "title",
-//         message: "What should be the title of the new file?",
-//       },
-//     ],
-//     actions: [
-//       {
-//         type: "add",
-//         path: "{{ turbo.paths.root }}/{{ dashCase file }}{{ type }}",
-//         templateFile: "templates/turborepo-generators.hbs",
-//       },
-//     ],
-//   });
-// }
-
 import type { PlopTypes } from "@turbo/gen";
+
+const turbo_root_path = "{{ turbo.paths.root }}";
+const src_path = "packages/blocks/src";
+const exporters_path = "packages/blocks";
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
   plop.setGenerator("block", {
@@ -60,29 +18,39 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
     actions: [
       {
         type: "add",
-        path: "{{ turbo.paths.root }}/packages/blocks/src/{{ name }}/{{ name }}.svelte",
+        path: `${turbo_root_path}/${src_path}/{{ name }}/{{ name }}.svelte`,
         templateFile: "templates/block.hbs",
       },
       {
         type: "add",
-        path: "{{ turbo.paths.root }}/packages/blocks/src/{{ name }}/config.ts",
+        path: `${turbo_root_path}/${src_path}/{{ name }}/config.ts`,
         templateFile: "templates/config.hbs",
       },
       {
         type: "add",
-        path: "{{ turbo.paths.root }}/packages/blocks/src/{{ name }}/index.ts",
+        path: `${turbo_root_path}/${src_path}/{{ name }}/graphql.ts`,
+        templateFile: "templates/graphql.hbs",
+      },
+      {
+        type: "add",
+        path: `${turbo_root_path}/${src_path}/{{ name }}/index.ts`,
         template: "export { default as {{ pascalCase name }} } from './{{ name }}.svelte'",
       },
       {
         type: "append",
-        path: "{{ turbo.paths.root }}/packages/blocks/blocks.ts",
+        path: `${turbo_root_path}/${exporters_path}/blocks.ts`,
         template:
           "export { default as {{ pascalCase name }} } from './src/{{ name }}/{{ name }}.svelte'",
       },
       {
         type: "append",
-        path: "{{ turbo.paths.root }}/packages/blocks/config.ts",
+        path: `${turbo_root_path}/${exporters_path}/config.ts`,
         template: "export { {{ pascalCase name }}Config } from './src/{{ name }}/config'",
+      },
+      {
+        type: "append",
+        path: `${turbo_root_path}/${exporters_path}/graphql.ts`,
+        template: "export { {{ upperCase name }} } from './src/{{ name }}/graphql'",
       },
     ],
   });
